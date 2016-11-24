89f8cc62ab4d4e00356ccd7c217f1c80f20cedc0
C80609A <c080609a@gmail.com>
2016-11-24 12:25:03 +0300

    develop: my_as_json for Floor.

--- app/models/c80_map_floors/floor.rb
+++ app/models/c80_map_floors/floor.rb
@@ -32,6 +32,22 @@ module C80MapFloors
       end
       res
     end
+    
+    def img_bg_url
+      res = nil
+      if img_bg.present?
+        res = img_bg.url
+      end
+      res      
+    end
+    
+    def img_overlay_url
+      res = nil
+      if img_overlay.present?
+        res = img_overlay.url
+      end
+      res      
+    end
 
     # private
 
@@ -40,6 +56,35 @@ module C80MapFloors
     def update_json
       Rails.logger.debug "[TRACE] <update_json> nope"
       # MapJson.update_json
+    end
+
+    def my_as_json
+
+      result = {
+          ord: self.ord,
+          id: self.id,
+          title: self.title,
+          tag: self.tag,
+          class_name: self.class_name,
+          map_building_id: self.map_building_id,
+          img_bg: {
+              url: self.img_bg_url
+          },
+          img_overlay: {
+              url: self.img_overlay_url
+          },
+          img_bg_width: img_bg_width,
+          img_bg_height: img_bg_height,
+          coords: self.coords,
+          areas: []
+      }
+
+      self.areas.each do |area|
+        # result[:areas] << area.my_as_json
+      end
+
+      result.as_json
+
     end
 
   end

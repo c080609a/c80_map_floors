89fe65d2232d09d533439e59faab1fa9e44f3b42
C80609A <c080609a@gmail.com>
2016-12-12 15:41:05 +0300

    develop_areas: major: route `fetch_unlinked_floors` comes on: вместе с AjaxController#fetch_unlinked_floors и вместе с view и партиалом _select_list_unlinked_floors.html.erb.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 C80MapFloors::Engine.routes.draw do
   match '/save_map_data', to: 'map_ajax#save_map_data', via: :post
   match '/ajax/map_edit_buttons', :to => 'ajax#map_edit_buttons', :via => :post
+
+  match '/ajax/fetch_unlinked_floors', to: 'ajax#fetch_unlinked_floors', via: :post
 end

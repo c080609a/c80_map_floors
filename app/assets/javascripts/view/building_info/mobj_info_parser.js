45873c5b2ff8f4f0767178169660ef7ffc3d5bc4
C80609A <c080609a@gmail.com>
2017-01-15 16:55:37 +0300

    develop_search_areas: * [X] при входе в Этаж без поиска: Реализовать отображение данных обо всех Арендаторах просматриваемого Этажа (также будут видны все свободные площади).

--- app/assets/javascripts/view/building_info/mobj_info_parser.js
+++ app/assets/javascripts/view/building_info/mobj_info_parser.js
@@ -13,8 +13,14 @@ function MobjInfoParser() {
     // текущий результат поиска - какие полигоны площадей соответствуют поиску и содержат искомые магазины
     var _sresults_areas = null;
 
-    // используется в _row_area
-    var _row_area_pattern = "<li><a href='{HREF}' title='{TITLE}' target='_blank'>{TITLE}</a> ({AREA_TITLE}), {PHONE}</li>";
+    // используется в _row_area_with_shop() и в _row_area_shop()
+    var _row_area_shop_pattern = "<li><a href='{HREF}' title='{TITLE}' target='_blank'>{TITLE}</a> ({AREA_TITLE}), {PHONE}</li>";
+
+    // используется в _row_area_data()
+    var _row_area_pattern = "<li><a href='{HREF}' title='{AREA_TITLE}' target='_blank'>{AREA_TITLE}</a></li>";
+
+    // режимы отображения данных - user mode. Их два: "режим арендатора" и "режим обычного пользователя"
+    var _umode = 'umode_simple_user'; // umode_arendator, umode_simple_user
 
     //--[ public ]------------------------------------------------------------------------------------------------------
 
@@ -146,7 +152,7 @@ function MobjInfoParser() {
 
                                 //#-> если всё в порядке (есть данные полигона площади по этому индексу) - лабаем из них html-строку
                                 if (i_json_area != undefined) {
-                                    result += _this._row_area(i_json_area);
+                                    result += _this._row_area_with_shop(i_json_area);
                                 }
 
                                 // иначе - сообщим об ошибке в лог
@@ -191,6 +197,14 @@ function MobjInfoParser() {
         _sresults_areas = sresult_areas;
     };
 
+    /** Извне задаём текущий режим просмотра данных.
+     *
+     * @param umode
+     */
+    this.setUmode = function (umode) {
+      _umode = umode;
+    };
+
     //--[ private ]-----------------------------------------------------------------------------------------------------
 
     /**
@@ -204,17 +218,56 @@ function MobjInfoParser() {
 
         var result = '';
 
-        // todo-umode:: на основе режима пользователя карты (Арендатор или ОбычныйПокупатель) отрисовать `просто данные` об Этаже
+        // todo-umode:: внедрить и использовать режим пользователя карты (Арендатор или ОбычныйПокупатель) и соответствующим образом отрисовать `просто данные` об Этаже
 
-        //<editor-fold desc=" // отобразим данные Этажа: метраж, кол-во площадей, коммуникации, цена за кв.м (режим Арендатора)">
-        result += _this._row('square', json_floor);
-        result += _this._row('square_free', json_floor);
-        //result += _this._row('floor_height', json);
-        result += _this._row('communications', json_floor);
-        result += _this._row('areas_count', json_floor);
-        result += _this._row('areas_free_count', json_floor);
-        result += _this._row('price_string', json_floor);
-        //</editor-fold>
+        if (_umode == 'umode_arendator') {
+            //<editor-fold desc=" // отобразим данные Этажа: метраж, кол-во площадей, коммуникации, цена за кв.м (режим Арендатора)">
+            result += _this._row('square', json_floor);
+            result += _this._row('square_free', json_floor);
+            //result += _this._row('floor_height', json);
+            result += _this._row('communications', json_floor);
+            result += _this._row('areas_count', json_floor);
+            result += _this._row('areas_free_count', json_floor);
+            result += _this._row('price_string', json_floor);
+            //</editor-fold>
+        }
+
+        else if (_umode == 'umode_simple_user') {
+
+            // отобразим всех арендаторов на этом этаже
+
+            var areas = json_floor['areas'];
+            if (areas != null) {
+
+                var len = areas.length;
+                var i, iar, iard; // i_area, i_area_data
+
+                for (i=0; i<len; i++) {
+
+                    // фиксируем данные полигона площади
+                    iar = areas[i];
+                    // фиксируем данные Площади, которая привязана к полигону
+                    iard = iar['data'];
+
+                    // если имеются данные Площади - работаем дальше (т.е. на выходе из if полюбому будет добавлена строка)
+                    if (iard != null) {
+
+                        // если имеются данные о Магазине (т.е. если есть Магазин, занимающий эту Площадь)
+                        if (iard['shop'] != null) {
+                            // соорудим строку, которая описывает этот Магазин
+                            result += _this._row_area_shop(iar);
+                        }
+
+                        // если же нет данных о Магазине (т.е. Площадь свободна)
+                        else {
+                            // соорудим строку, которая описывает эту Площадь
+                            result += _this._row_area_data(iard);
+                        }
+                    }
+
+                }
+            }
+        }
 
         return result;
     };
@@ -237,24 +290,62 @@ function MobjInfoParser() {
     };
 
     /**
-     * Выдать человекочитаемую html-строку <li>...</li>, которая описывает искомый Магазин.
+     * Соорудить html-строку, описывающую только Площадь (согласно паттерну _row_area_pattern).
+     * Т.е. даже если есть Магазин, занимающий эту Площадь - он не будет браться в рассчёт.
+     *
+     * Нет защиты от ошибок (это значит, что этот метод должен использоваться в if..else конструкции,
+     * которая проверяет наличие необходимых данных).
+     *
+     * @param area_data_json - это узел 'data' объекта, описывающего полигон площади.
+     * @private
+     */
+    this._row_area_data = function (area_data_json) {
+        var res = _row_area_pattern;
+        res = res.split('{HREF}').join('#'); // todo:: какой должен быть href у свободной Площади?
+        res = res.split('{AREA_TITLE}').join(area_data_json['title']);
+        return res;
+    };
+
+    /**
      * Этот служебный метод является контейнером кода, который желательно использовать только в makeHtmlText.
+     * Выдать человекочитаемую html-строку <li>...</li>, которая описывает искомый Магазин.
+     * Т.к. ВСЕГДА в таком случае ожидается наличие магазина - то в случае ошибки результат будет содержать
+     * сообщение об ошибке.
      *
      * @param area_json - данные об полигоне площади, сформированные методом C80MapFloors::Area.my_as_json4
      * @returns {string} - <li><a href='' title='' target='_blank'>Магазин такой-то</a>(площадь такая-то), телефоны<li>
      * @private
      *
      */
-    this._row_area = function (area_json) {
-        var res = '[ERROR] Нет данных о магазине в полигоне площади с id='+area_json['id'];
+    this._row_area_with_shop = function (area_json) {
+        var res = '<li>[ERROR] Нет данных о магазине в полигоне площади с id='+area_json['id']+'</li>';
         if (area_json['data'] != undefined && area_json['data']['shop'] != undefined) {
-            var shop = area_json['data']['shop'];
-            res = _row_area_pattern;
-            res = res.split('{TITLE}').join(shop['title']);
-            res = res.split('{HREF}').join(shop['url']);
-            res = res.split('{PHONE}').join(shop['tel']);
-            res = res.split('{AREA_TITLE}').join(area_json['data']['title']);
+            res = _this._row_area_shop(area_json);
         }
+        return res;
+    };
+
+    /**
+     * Соорудить строку, описывающую Магазин (согласно паттерну _row_area_shop_pattern).
+     *
+     * На вход подаётся json полигона площади.
+     * Нет защиты от ошибок (это значит, что этот метод должен использоваться в if..else конструкции,
+     * которая проверяет наличие необходимых данных)..
+     *
+     * Метод (контейнер кода) родился из метода _row_area_with_shop, когда начали внедрять режим _umode
+     * и расширять функционал _makeHtmlText_Umode_Floor.
+     *
+     * @param area_json
+     * @returns {string}
+     * @private
+     */
+    this._row_area_shop = function (area_json) {
+        var shop = area_json['data']['shop'];
+        var res = _row_area_shop_pattern;
+        res = res.split('{TITLE}').join(shop['title']);
+        res = res.split('{HREF}').join(shop['url']);
+        res = res.split('{PHONE}').join(shop['tel']);
+        res = res.split('{AREA_TITLE}').join(area_json['data']['title']);
         return res;
     };
 

d3e5ee441d12fb0f784ed54ece4e990c9f11198a
C80609A <c080609a@gmail.com>
2017-01-15 11:53:51 +0300

    develop_search_areas: MobjInfoParser: добавил результаты текущего поиска (_sresults_areas).

--- app/assets/javascripts/view/building_info/mobj_info_parser.js
+++ app/assets/javascripts/view/building_info/mobj_info_parser.js
@@ -2,14 +2,19 @@
 
 // в задачи этого кода входит преобразование JSON объектов в human-читаемый текст с характеристиками
 
-var MobjInfoParser = function () {
+function MobjInfoParser() {
 
     var _this = this;
 
     // с помощью этого хелпера можно понять, например, что square это "общая площадь" (типа locales/ru.yml)
     var _i18n = null;
 
-    /** Лабаем html текст из json-данных полигона Этажа/Здания/Площади
+    // todo-clear22:: необходимо сбрасывать это значение после того, как сбросили поиск
+    // текущий результат поиска - какие полигоны площадей соответствуют поиску и содержат искомые магазины
+    var _sresults_areas = null;
+
+    /**
+     * Лабаем html текст из json-данных полигона Этажа/Здания/Площади.
      *
      * @param json
      */
@@ -21,19 +26,94 @@ var MobjInfoParser = function () {
         //#-> предполагается, что json в поряде и данные целостны (т.е. уровнем выше была проверка json на корректность)
         switch (json["class_name"]) {
             case "C80MapFloors::Floor":
-                result += _this._row('square', json);
-                result += _this._row('square_free', json);
-                //result += _this._row('floor_height', json);
-                result += _this._row('communications', json);
-                result += _this._row('areas_count', json);
-                result += _this._row('areas_free_count', json);
-                result += _this._row('price_string', json);
+
+            /* json:
+            {
+                <...свойства картинки этажа, чисто класс C80MapFloors::Floor..>
+
+                "areas": [                                              // полигоны площадей этой картинки этажа
+                            {  "id": 5,
+                                <...C80MapFloors::Floor..>
+
+                                "data": {                               // данные привязанной к плоигону Площади
+                                    "id": 9,
+                                    "title": "Дом-1",
+                                    "square": null,
+                                    "desc": "",
+                                    "price_string": null,
+                                    "communications": null,
+                                    "is_free": false,
+                                    "shop": {                           // данные Магазина, который занимает эту Площадь
+                                        "id": 130,
+                                        "title": "ИП Кибардин",
+                                        "desc": "",
+                                        "tel": "(910) 514 08 68",
+                                        "site": "",
+                                        "url": "/shops/ip-kibardin.html"
+                                    }
+                                }
+                            },
+                ],
+
+                "data":                                                 // данные Этажа, привязанного к картинке этажа
+                    "id": 40,
+                    "ord": 1,
+                    "title": "1 этаж",
+                    "square": 0.0,
+                    "square_free": 0.0,
+                    "areas_count": 2,
+                    "areas_free_count": 2,
+                    "price_string": "от  до  руб./кв.м",
+                    "floor_height": null,
+                    "communications": ""
+                }
+            }*/
+
+            /* или сокращённо
+            json:
+            {
+                "areas": [                                              // полигоны площадей этой картинки этажа
+                            {  "id": 5,
+
+                                "data": {                               // данные привязанной к плоигону Площади
+                                    "id": 9,
+                                    "title": "Дом-1",
+                                    "is_free": false,
+                                    "shop": {                           // данные Магазина, который занимает эту Площадь
+                                        "id": 130,
+                                        "title": "ИП Кибардин",
+                                    }
+                                }
+                            },
+                ],
+
+                "data":                                                 // данные Этажа, привязанного к картинке этажа
+                    "id": 40,
+                    "title": "1 этаж",
+                }
+            }
+            */
+
+            //<editor-fold desc=" // отобразим данные Этажа: метраж, кол-во площадей, коммуникации, цена за кв.м">
+            result += _this._row('square', json);
+            result += _this._row('square_free', json);
+            //result += _this._row('floor_height', json);
+            result += _this._row('communications', json);
+            result += _this._row('areas_count', json);
+            result += _this._row('areas_free_count', json);
+            result += _this._row('price_string', json);
+            //</editor-fold>
+
             break;
         }
 
         result = "<ul>" + result + "</ul>";
         return result;
 
+    };
+
+    this.setSearchResultAreas = function (sresult_areas) {
+        _sresults_areas = sresult_areas;
     };
 
     this._row = function (key, json) {
@@ -54,5 +134,5 @@ var MobjInfoParser = function () {
     };
 
     _fInit();
-};
+}
 

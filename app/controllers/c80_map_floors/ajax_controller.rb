c7de39f742d21cbc2206549e106c9a1c66d541b5
C80609A <c080609a@gmail.com>
2017-05-24 15:15:55 +0300

    develop: перенёс поиск площади по категории в отдельный хелпер.

--- app/controllers/c80_map_floors/ajax_controller.rb
+++ app/controllers/c80_map_floors/ajax_controller.rb
@@ -4,6 +4,8 @@ module C80MapFloors
   # noinspection RubyResolve
   class AjaxController < ApplicationController
 
+    include AjaxHelper
+
     def map_edit_buttons
       # puts "<AjaxController.map_edit_buttons> #{params}"
     end
@@ -129,72 +131,7 @@ module C80MapFloors
 
       result = SearchResult.new
 
-      # 1. Находим категорию, title которой равен строке.
-      cats   = ::Category.where(:name => params[:stext])
-      if cats.count > 0
-        # работаем только с первой попавшейся категорией
-        cat = cats[0]
-
-        # если у категории имеются связанные магазины
-        if cat.leasers.count > 0
-
-          # переберём их
-          cat.leasers.each do |shop|
-
-            # добираемся до Rent::Area каждого магазина
-            if shop.areas.count > 0
-
-              shop.areas.each do |rent_area|
-
-                # если у Rent::Area имеется связанный полигон
-                if rent_area.area.present? # has_one
-
-                  map_area = rent_area.area
-
-                  # NOTE:: теперь, зная C80MapFloors::Area.id, находим id полигона этажа и id полигона здания
-
-                  Rails.logger.debug "[TRACE] <AjaxController.find_shops> add_area: #{map_area.id}"
-                  # result[:areas] << rent_area.area.id
-                  result.add_area(map_area.id)
-
-                  # посмотрим, имеется ли у полигона площади родители
-                  if map_area.floor.present?
-
-                    map_floor = map_area.floor
-                    Rails.logger.debug "[TRACE] <AjaxController.find_shops> add_floor: #{map_floor.id}"
-                    result.add_floor(map_floor.id)
-
-                    if map_floor.map_building.present?
-                      Rails.logger.debug "[TRACE] <AjaxController.find_shops> add_building: #{map_floor.map_building}"
-                      result.add_building(map_floor.map_building.id)
-                    else
-                      Rails.logger.debug "[TRACE] <AjaxController.find_shops> У Rent::Area нет родителей: #{rent_area.name}"
-                    end
-
-                  else
-                    Rails.logger.debug "[TRACE] <AjaxController.find_shops> У полигона площади нет родителей: #{rent_area.name}"
-                  end
-
-
-                else
-                  Rails.logger.debug "[TRACE] <AjaxController.find_shops> У Rent::Area нет полигона: #{rent_area.name}"
-                end
-
-              end
-
-            else
-              Rails.logger.debug "[TRACE] <AjaxController.find_shops> У магазина нет Rent::Area: #{shop.name}"
-            end
-
-          end
-
-        else
-          Rails.logger.debug "[TRACE] <AjaxController.find_shops> У этой категории нет магазинов: #{params[:stext]}"
-        end
-
-      else
-        Rails.logger.debug "[TRACE] <AjaxController.find_shops> Нет категории с таким именем = #{params[:stext]}"
-      end
+      find_areas_by_category!(result)
 
       Rails.logger.debug "[TRACE] <AjaxController.find_shops> Отправляем ответ: result = #{result.data}"
 
